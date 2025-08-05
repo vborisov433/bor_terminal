8c0b969cc4f67dae8bae05d886466ea225624b6e
vborisov433 <vborisov433@gmail.com>
2025-08-05 14:43:46 +0300

    refresh

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -3,10 +3,16 @@
 namespace App\Controller;
 
 use App\Repository\NewsItemRepository;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class NewsController extends AbstractController
@@ -40,18 +46,18 @@ final class NewsController extends AbstractController
         ]);
     }
 
-//    #[Route('/', name: 'app_news')]
-//    public function index(NewsItemRepository $repo): Response
-//    {
-//        $newsItems = $repo->createQueryBuilder('n')
-//            ->leftJoin('n.articleInfo', 'a')->addSelect('a')
-//            ->leftJoin('n.marketAnalyses', 'm')->addSelect('m')
-//            ->orderBy('n.id', 'DESC')
-//            ->getQuery()
-//            ->getResult();
-//
-//        return $this->render('news/index.html.twig', [
-//            'newsItems' => $newsItems
-//        ]);
-//    }
+    #[Route('/news/refresh', name: 'app_news_refresh', methods: ['POST'])]
+    public function refresh(KernelInterface $kernel, Request $request): JsonResponse
+    {
+        $app = new Application($kernel);
+        $app->setAutoExit(false);
+        $input = new ArrayInput(['command' => 'app:analyze-gpt']);
+        $output = new BufferedOutput();
+        $exitCode = $app->run($input, $output);
+
+        return new JsonResponse([
+            'success' => $exitCode === 0,
+            'output' => $output->fetch(),
+        ]);
+    }
 }
